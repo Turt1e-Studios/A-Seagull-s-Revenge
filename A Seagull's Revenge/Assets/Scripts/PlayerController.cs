c293ab30324b7f29109ae40db5dd1b88b3d3de20
Aishu85 <aishu.aravind5@gmail.com>
2022-11-22 14:49:58 -0800

    Enemy Movenment, Invisible Borders, Flipped Spriet

--- A Seagull's Revenge/Assets/Scripts/PlayerController.cs
+++ A Seagull's Revenge/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    float inputHorizontal;
     public float speed = 7;
+    bool facingRight = true;
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +17,29 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        Vector3 input = new Vector3(inputHorizontal, Input.GetAxisRaw("Vertical"), 0);
         Vector3 direction = input.normalized;
         Vector3 velocity = direction * speed;
         Vector3 moveAmount = velocity * Time.deltaTime;
 
         transform.position += moveAmount;
+
+        if (inputHorizontal > 0 && !facingRight)
+        {
+            Flip();
+        }
+        if (inputHorizontal < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+        facingRight = !facingRight;
     }
 }
